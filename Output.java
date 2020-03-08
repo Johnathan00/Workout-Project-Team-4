7b8354dba9398570de7fdc3b79ab70b699eadba3
cmdmac23 <61520148+cmdmac23@users.noreply.github.com>
2020-03-08 15:31:00 -0500

    Update Output.java

--- Output.java
+++ Output.java
@@ -5,179 +5,142 @@ package group_uno_workout;
  *
  * @author 
  */
+
 public class Output {
-//returns string for header with integer given in input
+
+    /**
+     * returns string for header with integer given in input
+     * 
+     * @param i integer indicating the round number
+     * @return string with html tags to print in header format
+     */
     public String print_header(int i){
         String header="<h1><u>Round " + i + "</u></h1>\n";
         return header;
     }
-//returns string of card with input of condensed version of card
+
+    /**
+     * returns string of card with input of condensed version of card
+     * 
+     * @param card the card from the hand, written in shorthand
+     * @return the card type written in more detail
+     */
     public String print_card(String card){
         
-        if("B0".equals(card)){
-            return "Blue 0";
-        }
-        else if("B1".equals(card)){
-            return "Blue 1";
-        }
-        else if("B2".equals(card)){
-            return "Blue 2";
-        }
-        else if("B3".equals(card)){
-            return "Blue 3";
-        }
-        else if("B4".equals(card)){
-            return "Blue 4";
-        }
-        else if("B5".equals(card)){
-            return "Blue 5";
-        }
-        else if("B6".equals(card)){
-            return "Blue 6";
-        }
-        else if("B7".equals(card)){
-            return "Blue 7";
-        }
-        else if("B8".equals(card)){
-            return "Blue 8";
-        }
-        else if("B9".equals(card)){
-            return "Blue 9";
-        }
-        else if("BD".equals(card)){
-            return "Blue Draw 2";
-        }
-        else if("BS".equals(card)){
-            return "Blue Skip ";
-        }
-        else if("BR".equals(card)){
-            return "Blue Reverse";
-        }
-        else if("G0".equals(card)){
-            return "Green 0";
-        }
-        else if("G1".equals(card)){
-            return "Green 1";
-        }
-        else if("G2".equals(card)){
-            return "Green 2";
-        }
-        else if("G3".equals(card)){
-            return "Green 3";
-        }
-        else if("G4".equals(card)){
-            return "Green 4";
-        }
-        else if("G5".equals(card)){
-            return "Green 5";
-        }
-        else if("G6".equals(card)){
-            return "Green 6";
-        }
-        else if("G7".equals(card)){
-            return "Green 7";
-        }
-        else if("G8".equals(card)){
-            return "Green 8";
-        }
-        else if("G9".equals(card)){
-            return "Green 9";
-        }
-        else if("GS".equals(card)){
-            return "Green Skip";
-        }
-        else if("GD".equals(card)){
-            return "Green Draw 2";
-        }
-        else if("GR".equals(card)){
-            return "Green Reverse";
-        }
-        else if("R0".equals(card)){
-            return "Red 0";
-        }
-        else if("R1".equals(card)){
-            return "Red 1";
-        }
-        else if("R2".equals(card)){
-            return "Red 2";
-        }
-        else if("R3".equals(card)){
-            return "Red 3";
-        }
-        else if("R4".equals(card)){
-            return "Red 4";
-        }
-        else if("R5".equals(card)){
-            return "Red 5";
-        }
-        else if("R6".equals(card)){
-            return "Red 6";
-        }
-        else if("R7".equals(card)){
-            return "Red 7";
-        }
-        else if("R8".equals(card)){
-            return "Red 8";
-        }
-        else if("R9".equals(card)){
-            return "Red 9";
-        }
-        else if("RS".equals(card)){
-            return "Red Skip";
-        }
-        else if("RD".equals(card)){
-            return "Red Draw 2";
-        }
-        else if("RR".equals(card)){
-            return "Red Reverse";
-        }
-        else if("Y0".equals(card)){
-            return "Yellow 0";
-        }
-        else if("Y1".equals(card)){
-            return "Yellow 1";
-        }
-        else if("Y2".equals(card)){
-            return "Yellow 2";
-        }
-        else if("Y3".equals(card)){
-            return "Yellow 3";
-        }
-        else if("Y4".equals(card)){
-            return "Yellow 4";
-        }
-        else if("Y5".equals(card)){
-            return "Yellow 5";
-        }
-        else if("Y6".equals(card)){
-            return "Yellow 6";
-        }
-        else if("Y7".equals(card)){
-            return "Yellow 7";
-        }
-        else if("Y8".equals(card)){
-            return "Yellow 8";
-        }
-        else if("Y9".equals(card)){
-            return "Yellow 9";
-        }
-         else if("YR".equals(card)){
-            return "Yellow Reverse";
-        }
-          else if("YD".equals(card)){
-            return "Yellow Draw 2";
-        }
-           else if("YS".equals(card)){
-            return "Yellow Skip";
-        }
-        else if("W1".equals(card)){
-            return "Wild";
-        }
-        else if("W4".equals(card)){
-            return "Wild 4";
-        }
-        else{
+        if(null == card){
             return "";
+        }
+        else switch (card) {
+            case "B0":
+                return "Blue 0";
+            case "B1":
+                return "Blue 1";
+            case "B2":
+                return "Blue 2";
+            case "B3":
+                return "Blue 3";
+            case "B4":
+                return "Blue 4";
+            case "B5":
+                return "Blue 5";
+            case "B6":
+                return "Blue 6";
+            case "B7":
+                return "Blue 7";
+            case "B8":
+                return "Blue 8";
+            case "B9":
+                return "Blue 9";
+            case "BD":
+                return "Blue Draw 2";
+            case "BS":
+                return "Blue Skip ";
+            case "BR":
+                return "Blue Reverse";
+            case "G0":
+                return "Green 0";
+            case "G1":
+                return "Green 1";
+            case "G2":
+                return "Green 2";
+            case "G3":
+                return "Green 3";
+            case "G4":
+                return "Green 4";
+            case "G5":
+                return "Green 5";
+            case "G6":
+                return "Green 6";
+            case "G7":
+                return "Green 7";
+            case "G8":
+                return "Green 8";
+            case "G9":
+                return "Green 9";
+            case "GS":
+                return "Green Skip";
+            case "GD":
+                return "Green Draw 2";
+            case "GR":
+                return "Green Reverse";
+            case "R0":
+                return "Red 0";
+            case "R1":
+                return "Red 1";
+            case "R2":
+                return "Red 2";
+            case "R3":
+                return "Red 3";
+            case "R4":
+                return "Red 4";
+            case "R5":
+                return "Red 5";
+            case "R6":
+                return "Red 6";
+            case "R7":
+                return "Red 7";
+            case "R8":
+                return "Red 8";
+            case "R9":
+                return "Red 9";
+            case "RS":
+                return "Red Skip";
+            case "RD":
+                return "Red Draw 2";
+            case "RR":
+                return "Red Reverse";
+            case "Y0":
+                return "Yellow 0";
+            case "Y1":
+                return "Yellow 1";
+            case "Y2":
+                return "Yellow 2";
+            case "Y3":
+                return "Yellow 3";
+            case "Y4":
+                return "Yellow 4";
+            case "Y5":
+                return "Yellow 5";
+            case "Y6":
+                return "Yellow 6";
+            case "Y7":
+                return "Yellow 7";
+            case "Y8":
+                return "Yellow 8";
+            case "Y9":
+                return "Yellow 9";
+            case "YR":
+                return "Yellow Reverse";
+            case "YD":
+                return "Yellow Draw 2";
+            case "YS":
+                return "Yellow Skip";
+            case "W1":
+                return "Wild";
+            case "W4":
+                return "Wild 4";
+            default:
+                return "";
         }
     }
 }
